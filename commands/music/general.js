db0e615b06d07f60dd73030fa7cc097b9b30da7d
RodariRogi23 <87240270+RodariRogi23@users.noreply.github.com>
2022-01-16 10:36:20 +0100

    Bug fixes

---
+++ commands/music/general.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name:"play",
+    aliases: ["p", "pause", "resume", "leave", "stop", "skip", "next", "autoplayoff", "autoplayon", "repeat", "queue"],
+    execute(message) {
+        return
+    }
+}
--- events/logs/delete.js
+++ events/logs/delete.js
@@ -3,8 +3,9 @@ module.exports = {
     execute(message) {
         const channel = client.channels.cache.get(config.idcanali.logs)
         let user = message.author
+        if(message.attachment) return
         let embed = new Discord.MessageEmbed()
-            .addField(`Messaggio eliminato:`, message.toString())
+            .addField("Contenuto:", message.content ? (message.content.length > 500 ? `${message.content.slice(0, 497)}...` : message.content) : "_Nessun Contenuto_")
             .setAuthor({name: user.tag.toString(), iconURL: message.member.displayAvatarURL({dynamic: true})})
             .setFooter({text: `User ID: ${message.author.id}`})
             .setDescription(`Messaggio eliminato in ${message.channel}`)
