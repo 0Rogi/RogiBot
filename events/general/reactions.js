d1caf60db621de1bd396c4ccbe3002ba0b9eac71
RodariRogi23 <87240270+RodariRogi23@users.noreply.github.com>
2022-01-22 13:46:09 +0100

    Bug Fix

--- events/general/reactions.js
+++ events/general/reactions.js
@@ -1,15 +1,14 @@
 module.exports = {
     name: `messageCreate`,
     execute(message) {
-        if(message.content.includes(`:`) || message.author.bot) return
-        if(message.content.toLowerCase().includes(`rogi`)) message.react(`<:Rogi:904005869799366696>`)
-        if(message.content.toLowerCase().includes(`ciao`) || message.content.toLowerCase().includes(`salve`) || message.content.toLowerCase().includes(`hello`) || message.content.toLowerCase().includes(`benvenuto`)) message.react(`<:RogiCiao:904001520880726036>`)
-        if(message.content.toLowerCase().includes(`?`)) message.react(`<:RogiDomandoso:904007044246433923>`)
-        if(message.content.toLowerCase() == `f`) message.react(`<:RogiF:904002259921293382>`)
-        if(message.content.toLowerCase() == `gg`) message.react(`<:RogiGG:904002692286922753> `)
-        if(message.content.toLowerCase().includes(`amore`) || message.content.toLowerCase().includes(`adoro`) || message.content.toLowerCase().includes(`adorare`)) message.react(`<:RogiHearts:904005290905718874>`)
-        if(message.content.toLowerCase().includes(`lol`)) message.react(` <:RogiLOL:904003061960282142> `)
-        if(message.content.toLowerCase().includes(`ok`) || message.content.toLowerCase().includes(`va bene`) || message.content.toLowerCase().includes(`capito`)) message.react(`<:RogiOk:904003454350004265>`)
-        if(message.content.toLocaleLowerCase().includes(`triste`) || message.content.toLowerCase().includes(`sad`) || message.content.toLowerCase().includes(`piango`)) message.react(`<:RogiSad:904004495611142176>`)
+        if(message.content.toLowerCase() == `rogi`) return message.react.react(`<:Rogi:904005869799366696>`)
+        if(message.content.toLowerCase() == `ciao` || message.content.toLowerCase() == `salve` || message.content.toLowerCase() == `hello` || message.content.toLowerCase() == `benvenuto`) return message.react.react(`<:RogiCiao:904001520880726036>`)
+        if(message.content.toLowerCase().includes(`?`)) return message.react.react(`<:RogiDomandoso:904007044246433923>`)
+        if(message.content.toLowerCase() == `f`) return message.react.react(`<:RogiF:904002259921293382>`)
+        if(message.content.toLowerCase() == `gg`) return message.react.react(`<:RogiGG:904002692286922753> `)
+        if(message.content.toLowerCase() == `amore` || message.content.toLowerCase() == `adoro`) return message.react.react(`<:RogiHearts:904005290905718874>`)
+        if(message.content.toLowerCase() == `lol`) return message.react.react(` <:RogiLOL:904003061960282142> `)
+        if(message.content.toLowerCase() == `ok` || message.content.toLowerCase() == `va bene` || message.content.toLowerCase() == `k` || message.content.toLowerCase() == `capito`) return message.react.react(`<:RogiOk:904003454350004265>`)
+        if(message.content.toLocaleLowerCase() == `triste` || message.content.toLowerCase() == `sad` || message.content.toLowerCase() == `piango`) return message.react.react(`<:RogiSad:904004495611142176>`)
     }
 }
