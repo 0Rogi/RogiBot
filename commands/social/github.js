ffaa05251a40da2cc53c0ba3cac148a65b0d5f6c
Rogi <0Rogi@users.noreply.github.com>
2022-02-24 19:43:36 +0100

    Update github.js

--- commands/social/github.js
+++ commands/social/github.js
@@ -3,11 +3,11 @@ module.exports = {
     execute(message) {
         let embed = new Discord.MessageEmbed()
             .setTitle(`Rogi`)
-            .setURL(`https://github.com/RodariRogi23`)
+            .setURL(`https://github.com/0Rogi`)
             .setColor(`YELLOW`)
-            .setThumbnail(`https://cdn3.iconfinder.com/data/icons/inficons/512/github.png`)
+            .setThumbnail(`https://github.com/0Rogi`)
             .setDescription(`:v: Questo è l'account **github** di Rogi\nse ti va, facci un salto!`)
-        let row = new Discord.MessageActionRow().addComponents(new Discord.MessageButton().setLabel(`GITHUB`).setStyle(`LINK`).setURL(`https://github.com/RodariRogi23`).setEmoji(`🖊️`))
+        let row = new Discord.MessageActionRow().addComponents(new Discord.MessageButton().setLabel(`GITHUB`).setStyle(`LINK`).setURL(`https://github.com/0Rogi`).setEmoji(`🖊️`))
         message.reply({embeds: [embed], components: [row]})
     }
 }
