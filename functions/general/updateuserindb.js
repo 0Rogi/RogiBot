a24b5a923c9500ca340fa4c6812a4ed0a935d538
Rogi <0Rogi@users.noreply.github.com>
2022-07-26 17:52:39 +0200

    Fix Bugs

--- functions/general/updateuserindb.js
+++ functions/general/updateuserindb.js
@@ -2,6 +2,7 @@ const config = require(`${process.cwd()}/JSON/config.json`)
 
 module.exports = function updateuserindb() {
     client.guilds.cache.get(config.idServer.idServer).members.cache.forEach(member => {
+        if (member.roles.cache.has(config.idruoli.unverified)) return
         database.collection(`UserStats`).find({ id: member.id }).toArray(function (err, result) {
             if (!result[0]) {
                 database.collection(`UserStats`).insertOne({ id: member.id, username: member.user.username, roles: member._roles, moderation: {}, leavedAt: 0 })
