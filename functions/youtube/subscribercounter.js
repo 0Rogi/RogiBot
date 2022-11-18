dc7d6728702b746e299e496ddbcfab39682530ad
Rogi <0Rogi@users.noreply.github.com>
2022-11-18 19:58:50 +0100

    Fix Bugs

--- functions/youtube/subscribercounter.js
+++ functions/youtube/subscribercounter.js
@@ -2,7 +2,8 @@ const { getChannelInfo } = require(`yt-channel-info`)
 const config = require(`${process.cwd()}/JSON/config.json`)
 
 module.exports = function subscribercounter() {
-    getChannelInfo(`UCw7lKb-XBW4ApE0puSbJLFQ`).then((response) => {
+    const payload = { channelId: `UCw7lKb-XBW4ApE0puSbJLFQ`, channelIdType: 0 };
+    getChannelInfo(payload).then((response) => {
         let canaleyoutube = client.channels.cache.get(config.idcanali.iscritti)
         canaleyoutube.setName(`—͟͞͞🎬】Subscribers: ${response?.subscriberCount}`)
     })
