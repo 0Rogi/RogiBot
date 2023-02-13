2530c80c0ff5189d0001eb8d0a41b7989fa38468
Rogi <0Rogi@users.noreply.github.com>
2023-02-13 20:27:08 +0100

    Fix Bugs

--- events/general/reactions.js
+++ events/general/reactions.js
@@ -56,28 +56,28 @@ module.exports = {
 
         let greetings = [`salve`, `ciao`, `hi`, `buongiorno`, `hey`, `we`, `hola`, `addio`, `hello`];
         if (greetings.includes(content)) {
-            message.react(`<: RogiCiao: 1043918448134193235 > `);
+            message.react(`<:RogiCiao:1043918448134193235>`);
             return;
         }
 
         let sadness = [`triste`, `sad`, `piango`, `piange`];
         if (sadness.includes(content)) {
-            message.react(`<: RogiSAD: 1043918454723461210 > `);
+            message.react(`<:RogiSAD: 1043918454723461210>`);
             return;
         }
 
         if (content == `f`) {
-            message.react(`<: RogiF: 1043918450210373682 > `);
+            message.react(`<:RogiF:1043918450210373682>`);
             return;
         }
 
         if (content == `gg`) {
-            message.react(`<: RogiGG: 1043918451879727174 > `);
+            message.react(`<:RogiGG:1043918451879727174>`);
             return;
         }
 
         if (content == `lol`) {
-            message.react(`<: RogiLOL: 1043918453322567680 > `);
+            message.react(`<:RogiLOL:1043918453322567680>`);
             return;
         }
 
